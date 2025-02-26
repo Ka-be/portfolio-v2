8dda8dc651da5ba06265d909feeb3bdb89b1373e
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2025-02-26 15:17:10 +0100

    Add lucid-react dependency + Test navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,11 @@
+import { Smile } from "lucide-react";
+
 const Navbar = () => {
 	return (
-		<nav className="w-full bg-slate-600 h-8">
-			Navbar
+		<nav className="w-full bg-slate-800 h-12 flex justify-evenly items-center">
+			<div className="flex justify-evenly items-center">
+				<p>Navbar</p>
+			</div>
 		</nav>
 	);
 };
