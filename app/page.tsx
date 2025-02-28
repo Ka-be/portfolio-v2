45e4f23cfe9e2c040616988ad344c37a04854bef
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2025-02-28 15:53:35 +0100

    Add animated icon and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
-import TestContent from "@/components/TestContent";
-import TestCarousel from "@/components/TestCarousel";
+import Footer from "@/components/Footer";
+import AnimatedLogo from "@/components/UI/AnimatedLogo";
 
 export default function Home() {
   return (
-    <div className="min-h-screen font-[family-name:var(--font-geist-sans)]">
+    <div className="min-h-screen font-[family-name:var(--font-geist-sans)] flex flex-col items-center justify-center">
+      <AnimatedLogo />
       <h1>Work in progress</h1>
+      <Footer />
     </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="flex justify-center items-center h-10 absolute bottom-0 w-full">
+            <p>Footer baby</p>
+        </footer>
+    );
+};
+
+export default Footer;
