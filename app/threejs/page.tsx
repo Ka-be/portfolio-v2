0cf5c477d502a1cab07f499f5bd548a66e46c363
Kevin Bourgitteau <kevin.bourgitteau@gmail.com>
2025-03-04 19:01:44 +0100

    tests three.js

--- app/threejs/page.tsx
+++ app/threejs/page.tsx
@@ -1,13 +1,13 @@
-
 import TestContent from "@/components/TestContent";
 import Overlay from "@/components/ui/Overlay";
+import ThreeComponent from "@/components/ui/ThreeComponent";
 
 export default function Home() {
 	return (
 		<>
 			<Overlay />
 			<div className="h-screen-dynamic md:h-full w-full font-lexend flex flex-col">
-				<TestContent textContent="Test ThreeJS" />
+				<ThreeComponent /> 
 			</div>
 		</>
 	);
---
+++ components/UI/ThreeComponent.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+// https://www.youtube.com/watch?v=vTfMjI4rVSI
+
+import { useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+
+
+const Sphere = ({ position, radius, color, args }: { position: [number, number, number], radius: number, color: string, args: [number, number, number] }) => {
+    const ref = useRef<import('three').Mesh>(null);
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    useFrame((state, delta) => {
+        if (ref.current) {
+            ref.current.rotation.x += delta;
+            ref.current.rotation.y += delta;
+        }
+    });
+    return (
+        <mesh 
+        position={position} 
+        ref={ref} 
+        onPointerEnter={(event) => (event.stopPropagation(), setIsHovered(true))}
+        onPointerLeave={() => setIsHovered(false)}
+        >
+            <sphereGeometry args={args}  />
+            <meshStandardMaterial color={isHovered ? 'red' : color} wireframe={true} />
+        </mesh>
+    );
+};
+
+
+const ThreeComponent = () => {
+    return (
+        <div className="min-h-screen flex items-center flex-col justify-center text-xl font-bold">
+            <p>Test ThreeJS</p>
+            <Canvas style={{ width: '100vw', height: '50vh' }}>
+                <directionalLight position={[1, 1, 2]} />
+                <ambientLight intensity={0.3} />
+                <Sphere position={[0, 0, 0]} radius={1} color={'white'} args={[1, 32, 20]} /> 
+
+            </Canvas>
+        </div>
+    );
+};
+
+export default ThreeComponent;
