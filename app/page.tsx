9ff87fd2813d9199792f6cb7641e151a9a0ae913
ka-be <kevin.bourgitteau@gmail.com>
2025-03-18 09:07:44 +0000

    tests on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
-import AnimatedLogo from "@/components/UI/AnimatedLogo";
-import TestContent from "@/components/TestContent";
-import TestCarousel from "@/components/TestCarousel";
 import Overlay from "@/components/UI/Overlay";
-
+import WaveSurface from "@/components/UI/WaveSurface";
 export default function Home() {
 	return (
 		<>
 			<Overlay />
 			<div className="h-screen-dynamic md:h-full w-full font-lexend flex flex-col">
-				<TestContent textContent="Moi c'est Kevin." />
-				<TestCarousel />
-				<TestContent textContent="Et c'est déjà pas mal." />
+				{/* <WaveSurface /> */}
+				<div className="absolute top-0 left-0 w-full h-full"
+					style={{
+						top: 'var(--frame-size)',
+						left: 'var(--frame-size)'
+					}}
+				>
+					<h1 className="text-5xl font-light text-foreground uppercase tracking-widest ml-10 mt-10">
+						Kevin <br /> Bourgitteau
+					</h1>
+					<h2 className="text-2xl font-light text-foreground tracking-widest ml-10">
+						Développeur Full-Stack, Bordeaux
+					</h2>
+				</div>
 			</div>
 		</>
 	);
