19ade24e1b27a2ebcbb3bf708e511884a0bda8b3
ka-be <kevin.bourgitteau@gmail.com>
2025-03-18 10:15:38 +0000

    Test with projects

--- data/projects.ts
+++ data/projects.ts
@@ -3,16 +3,16 @@ export const projects = [
       id: 1,
       title: "Mon super projet",
       description: "Une brève description du projet.",
-      image: "/images/projet1.jpg",
-      link: "https://monprojet.com",
+      image: "https://dummyimage.com/600x400/000/fff&text=1",
+      link: "/",
       technos: ["Next.js", "Tailwind", "Framer Motion"]
     },
     {
       id: 2,
       title: "Un autre projet",
       description: "Encore un projet cool.",
-      image: "/images/projet2.jpg",
-      link: "https://autreprojet.com",
+      image: "https://dummyimage.com/600x400/000/fff&text=2",
+      link: "/",
       technos: ["React", "TypeScript"]
     }
   ];
