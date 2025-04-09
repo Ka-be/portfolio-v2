a36da012a835fed06d8f1b02db4a2acb3efbd6d7
ka-be <kevin.bourgitteau@gmail.com>
2025-04-09 13:56:03 +0000

    add timeline.ts to prepare data

---
+++ data/timeline.ts
@@ -0,0 +1,37 @@
+export const timelineData = [
+  {
+    id: 1,
+    title: "2024",
+    content: "Content for 2024",
+  },
+  {
+    id: 2,
+    title: "Early 2023",
+    content: "Content for Early 2023",
+  },
+  {
+    id: 3,
+    title: "Changelog",
+    content: "Content for Changelog",
+  },
+  {
+    id: 4,
+    title: "Changelog",
+    content: "Content for Changelog",
+  },
+  {
+    id: 5,
+    title: "Changelog",
+    content: "Content for Changelog",
+  },
+  {
+    id: 6,
+    title: "Changelog",
+    content: "Content for Changelog",
+  },
+  {
+    id: 7,
+    title: "Changelog",
+    content: "Content for Changelog",
+  },
+];
