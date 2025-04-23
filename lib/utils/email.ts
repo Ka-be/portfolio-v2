38b49f8a513bee51e32d22634e23a613fb355ea0
ka-be <kevin.bourgitteau@gmail.com>
2025-04-23 12:37:46 +0000

    error gestion

--- lib/utils/email.ts
+++ lib/utils/email.ts
@@ -1,28 +1,29 @@
 import emailjs from '@emailjs/browser'
 
 interface ContactFormInput {
-  name: string
-  email: string
-  message: string
+    name: string
+    email: string
+    message: string
 }
 
 export const sendEmail = async (formData: ContactFormInput): Promise<void> => {
-  const { name, email, message } = formData
+    const { name, email, message } = formData
 
-  const templateParams = {
-    from_name: name || 'Anonyme',
-    reply_to: email,
-    message: message,
-  }
+    const templateParams = {
+        from_name: name || 'Anonyme',
+        reply_to: email,
+        message: message,
+    }
 
-  try {
-    await emailjs.send(
-      process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
-      process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
-      templateParams,
-      process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
-    )
-  } catch (error) {
-    throw new Error('Erreur lors de l’envoi du mail via EmailJS')
-  }
+    try {
+        await emailjs.send(
+            process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!,
+            process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!,
+            templateParams,
+            process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+        )
+    } catch (error: any) {
+        console.error('Erreur lors de l\'envoi du mail via EmailJS:', error)
+        throw new Error('Erreur lors de l\'envoi du mail via EmailJS')
+    }
 }
